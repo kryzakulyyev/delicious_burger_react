f93afef1032011d0b3961bcc69eddbec6f5fa21d
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2021-01-03 12:56:51 -0800

    Convert App Component to class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React from 'react'
 import Login from './components/Login'
 import logo from './burger-logo.jpg';
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component {
+  render(){
+
   return (
     <div className="App">
       <img src={logo} alt="Delicious Burger" id="logo" />
       <hr></hr>
       <Login />
-
     </div>
   );
+  }
 }
 
 export default App;
