44534beccf9330490f7b77bbfa3f78fe6a04f335
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2021-01-03 15:30:20 -0800

    set propertys order page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,19 +3,19 @@ function Login(props) {
     props.userName(e.target.value)
   }
   function handlePassword(e){
-    props.userPassword(e.target.value)
+    props.userpassword(e.target.value)
   }
   return (
     <form>
-    <h1>Login to start creating a burger!</h1>
-    <label htmlFor="username">Username: </label>
-    <input type="text" name="username" id="username" autoComplete='off' onInput={handleUsername}/>
-    <br/>
-    <label for="user-pw">Password: </label>
-    <input id="user-pw" type="password" name="user-pw" onInput={handlePassword}/>
-    <br/>
-    <input type="button" value="Login" onClick={props.checkUser}/>
-  </form>
+      <h1>Login to start creating a burger!</h1>
+      <label htmlFor="username">Username: </label>
+      <input type="text" name="username" id="username" autoComplete="off" onInput={handleUsername} />
+      <br/>
+      <label htmlFor="user-pw">Password: </label>
+      <input id="user-pw" type="password" name="user-pw" onInput={handlePassword} />
+      <br/>
+      <input type="button" value="Login" onClick={props.checkUser}/>
+    </form>
   )
 }
 
